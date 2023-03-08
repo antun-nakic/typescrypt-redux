2cf0f711325dca793c3901bbec5c4e6cf5d76cf5
Antun-Nakic <antun.nakic@digital-abundance.hr>
2023-03-08 20:48:54 +0100

    ostvaren redux thunk

--- src/components/Unuk.tsx
+++ src/components/Unuk.tsx
@@ -2,26 +2,38 @@ import { useContext } from "react";
 import { ColorContext } from "../context/ColorContext";
 import { ThemeContext } from "../context/ThemeContext";
 
-import { useAppSelector, useAppDispatch } from '../store/hooks';
-import { izbaciPrviFilm,addMovie } from '../store/slices/MoviesSlice'
+import { useAppSelector, useAppDispatch } from "../store/hooks";
+import { izbaciPrviFilm, addMovie } from "../store/slices/MoviesSlice";
 
 const Unuk = () => {
-    const movies = useAppSelector((state) => state.movies)
-    const colorLightness = useContext(ColorContext);
-    const theme = useContext(ThemeContext);
+  const movies = useAppSelector((state) => state.movies);
+  const colorLightness = useContext(ColorContext);
+  const theme = useContext(ThemeContext);
 
-    const dispatch = useAppDispatch()
+  const dispatch = useAppDispatch();
 
-    return (
-        <div style={{backgroundColor: `hsl(${theme} 100% ${colorLightness}%)`}}>    
-            <h3>Ovo trenutno imamo u centralnom skladištu stanja, slice movies</h3>
-            <ul>
-            {movies.map((clan,index)=><li key={index}>{clan}</li>)}
-            </ul>
-            <button onClick={()=>dispatch(izbaciPrviFilm())}>Izbaci prvog</button>
-            <button onClick={()=>dispatch(addMovie("dozvola za brak"))}>Dodaj "dozvola za brak"</button>
-        </div>
-    )
-}
-    
-export default Unuk
+  return (
+    <div style={{ backgroundColor: `hsl(${theme} 100% ${colorLightness}%)` }}>
+      <h3>Ovo trenutno imamo u centralnom skladištu stanja, slice movies</h3>
+      {movies.status === "idle" ? (
+        <>
+          <ul>
+            {movies.results.map((clan, index) => (
+              <li key={index}>{clan}</li>
+            ))}
+          </ul>
+          <button onClick={() => dispatch(izbaciPrviFilm())}>
+            Izbaci prvog
+          </button>
+          <button onClick={() => dispatch(addMovie("dozvola za brak"))}>
+            Dodaj "dozvola za brak"
+          </button>
+        </>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </div>
+  );
+};
+
+export default Unuk;
--- src/store/slices/MoviesSlice.ts
+++ src/store/slices/MoviesSlice.ts
@@ -1,20 +1,52 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction, createAsyncThunk } from "@reduxjs/toolkit";
 import type { RootState } from "../store";
 
-type MoviesState = string[];
+export const fetchMovies = createAsyncThunk(
+  "movies/fetchMovies",
+  async (name: string) => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/search/movie?query=${name}&api_key=ab8596081145bffa991ffe5317e1d3c4`,
+      {
+        headers: {
+          accept: "application/json, text/plain, */*",
+        },
+        body: null,
+        method: "GET",
+      }
+    );
+    let data = await response.json();
 
-const initialState: MoviesState = ["Umri muski", "Brave heart"];
+    let finalData = data.results.map((clan: { title: string }) => clan.title);
+    return finalData;
+  }
+);
+
+type MoviesState = { status: string; results: string[] };
+
+const initialState: MoviesState = {
+  status: "idle",
+  results: ["Umri muski", "Brave heart"],
+};
 
 export const moviesSlice = createSlice({
   name: "movies",
   initialState,
   reducers: {
     izbaciPrviFilm: (state) => {
-      state.shift();
+      state.results.shift();
     },
     addMovie: (state, action: PayloadAction<string>) => {
-      state.push(action.payload);
+      state.results.push(action.payload);
     },
+  },
+  extraReducers(builder) {
+    builder.addCase(fetchMovies.fulfilled, (state, { payload }) => {
+      state.status = "idle";
+      state.results.push(...payload);
+    });
+    builder.addCase(fetchMovies.pending, (state, { payload }) => {
+      state.status = "loading";
+    });
   },
 });
 
